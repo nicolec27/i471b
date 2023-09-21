d32cceb64dc73da3cf9a9b0b3210113812ded9e0
Nicole Chan <nchan14@binghamton.edu>
2023-09-21 17:31:24 -0400

    completed exercise2

--- submit/lab4/lab4-sol.scm
+++ submit/lab4/lab4-sol.scm
@@ -13,6 +13,20 @@
 (define (ls-prod ls1 ls2)
 	(if (null? ls1)
 		'()
-	(cons (> (car ls1) (car ls2))
-		(ls-prod (cdr ls1) (cdr ls2)))))			
+	(cons (* (car ls1) (car ls2))
+		(ls-prod (cdr ls1) (cdr ls2)))))	
+
+(define ls-f
+    (lambda (ls1 ls2 (fn (lambda (a b) (+ a b))))
+	(if (null? ls1)
+		'()
+    (cons (fn (car ls1) (car ls2))
+		(ls-f (cdr ls1) (cdr ls2) fn)))))
 		
+(define (greater-than ls1 v)
+	(if (null? v)
+		0
+	(if (null? ls1)
+		'()
+		(cons (> (car ls1) v)
+			(greater-than (cdr ls1) v)))))
