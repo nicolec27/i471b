f09002f13446cce78043530c26dc5fbd88ca5075
Nicole Chan <nchan14@binghamton.edu>
2023-11-16 22:24:40 -0500

    started lab8

---
+++ submit/lab8/lab8_sol.erl
@@ -0,0 +1,24 @@
+-module(lab8_sol).
+
+% this attribute exports all functions; meant for devel work, but
+% we use in this project to avoid having the add our functions to
+% an export list.
+-compile(export_all).
+
+% Use this function to grab some test data: Shapes = lab6:shapes_data().
+shapes_data() ->
+  [ { square, 2 }, { circle, 1 }, { square, 1 },
+    { square, 3 }, { circle, 2 }, { square, 4 }
+  ].
+
+% Use this function to grab some test data: Grades = lab6:grades_data().
+grades_data() ->
+  [ {bill, 82.0}, {sue, 95}, { john, 85},
+    { joe, 73 }, { mary, 65}, { tom, 55}
+  ].
+
+
+%% Exercise 2
+
+%% Exercise 3.
+
